9a4c49e209292efeeb243d2971610e13a5da9109
Khulan <khulan.msc.2012@gmail.com>
2017-10-24 21:02:29 +0200

    Update readFile.sh
    Change in curl command

--- readFile.sh
+++ readFile.sh
@@ -4,6 +4,6 @@ file="proxies.txt"
 current_date_time=$(date '+%Y/%m/%d %H:%M')
 while IFS= read -r proxy
 do
-        { echo -n $current_date_time ; echo -n '\t' ; echo  $(curl -m 180 -w %{$
+        { echo -n $current_date_time ; echo -n '\t' ; echo  $(curl -x 10.138.0.2:3128 -U "david.pinilla":"|Jn 5DJ\\7inbNniK|m@^ja&>C" -m 180 -w %{time_total},%{http_code} -H Cache-control:private http://ovh.net/files/1Mb.dat) }
 done <"$file"
 
