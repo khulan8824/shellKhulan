3173f195af002a6a5c36a7523187d6ab021f7d58
khulan8824 <khulan.msc.2012@gmail.com>
2017-10-25 04:57:58 -0400

    Querying proxy

---
+++ readProxy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+file="proxies.txt"
+current_date_time=$(date '+%Y/%m/%d %H:%M')
+while IFS= read -r proxy
+	do
+		{ echo -n $current_date_time ; echo -n ',' ; echo  $(curl -x $proxy -U "david.pinilla":"|Jn 5DJ\\7inbNniK|m@^ja&>C" -m 180 -w %{time_total},%{http_code} -H Cache-control:private http://ovh.net/files/1Mb.dat -o /dev/null -s) ;} >>$proxy
+	done <"$file"
